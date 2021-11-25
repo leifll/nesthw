0a47e1eefa0bcd9af2481e55acd426ad380fa98a
Leif <leifl@kth.se>
2021-11-25 15:37:00 +0100

    docker locally working

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+// import { AppService } from './app.service';
 
-@Controller()
+@Controller('/')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('nesthw')
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hello from nest';
   }
 }
